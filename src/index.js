50b88352bd939962608a31d3296dbba8d273258e
Graziano Blasilli <33848564+blasilli@users.noreply.github.com>
2021-04-11 21:54:10 +0200

    Revert "."
    This reverts commit fa50e9857f84e7e1c1c527d8c7d58cd8ddbcdea3.

--- src/index.js
+++
@@ -1,6 +0,0 @@
-export { default as radviz } from './radviz';
-export { radvizDA as radvizDA } from './arrangement';
-/*
-export { heuristicArrangement as heuristicArrangement }
-from './heuristic'
-*/
